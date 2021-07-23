e95463771b4c2f1151d602f348f2922912369f0a
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-07-23 02:32:53 -0400

    Add main functionality

--- killfam_unix.go
+++ killfam_unix.go
@@ -4,6 +4,7 @@ package killfam
 
 import (
 	"os/exec"
+	"syscall"
 )
 
 // Augment configures the process of the command such that its entire process
@@ -11,12 +12,22 @@ import (
 //
 // Augment must be run after the command is created with exec.Command, but
 // before the command is executed.
-func Augment(cmd *exec.Cmd) error {
-	return nil
+func Augment(cmd *exec.Cmd) {
+	// Adapted from:
+	// https://stackoverflow.com/a/29552044/1376127
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
 
 // KillTree kills the entire process tree of the input process, assuming it was
 // augmented using killfam.Augment.
 func KillTree(cmd *exec.Cmd) error {
+	// Adapted from:
+	// https://stackoverflow.com/a/29552044/1376127
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		return err
+	}
+
+	syscall.Kill(-pgid, syscall.SIGTERM)
 	return nil
 }
--- killfam_windows.go
+++ killfam_windows.go
@@ -4,6 +4,7 @@ package killfam
 
 import (
 	"os/exec"
+	"strconv"
 )
 
 // Augment configures the process of the command such that its entire process
@@ -11,12 +12,15 @@ import (
 //
 // Augment must be run after the command is created with exec.Command, but
 // before the command is executed.
-func Augment(cmd *exec.Cmd) error {
-	return nil
+func Augment(cmd *exec.Cmd) {
+	// Does nothing on Windows
 }
 
 // KillTree kills the entire process tree of the input process, assuming it was
 // augmented using killfam.Augment.
 func KillTree(cmd *exec.Cmd) error {
-	return nil
+	// Adapted from:
+	// https://stackoverflow.com/a/44551450/1376127
+	kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(cmd.Process.Pid))
+	return kill.Run()
 }
